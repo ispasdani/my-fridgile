abf69b9fde5d72862d47c57ae862ba30a12f5ede
ispasdani <ispasdani14@gmail.com>
2022-10-22 15:58:24 +0200

    deleted a few components

--- src/pages/MyFridge/MyFridge.jsx
+++ src/pages/MyFridge/MyFridge.jsx
@@ -7,10 +7,6 @@ import Meat from "../../assets/images/meat.svg";
 import Fish from "../../assets/images/fish.svg";
 import Plus from "../../assets/images/plus.svg";
 import "./myFridge.css";
-import AddCard from "../../components/AddCard/AddCard.jsx";
-import CatSearch from "../../components/CarSearch/CatSearch";
-import AppWidget from "../../components/AppWidget/AppWidget.jsx";
-import RecipeWidget from "../../components/RecipeWidget/RecipeWidget.jsx";
 
 export function Myfridge() {
   return (
@@ -20,13 +16,10 @@ export function Myfridge() {
           <p>My fridge</p>
         </div>
 
-        <CatSearch />
         <div className="body-text">
           <p>Click on the category to see and add items.</p>
         </div>
         <div className="grid-container">
-          <AddCard icon={Plus} />
-
           <Card icon={Carrot} title="Vegetables" />
 
           <Card icon={Banana} title="Fruits" />
@@ -38,10 +31,7 @@ export function Myfridge() {
           <Card icon={Fish} title="Fish and seafood" />
         </div>
       </div>
-      <div className="right-side">
-        <AppWidget />
-        <RecipeWidget />
-      </div>
+      <div className="right-side"></div>
     </div>
   );
 }
